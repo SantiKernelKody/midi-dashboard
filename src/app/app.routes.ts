73e34c4cbc8d86b9dba59e3ae5b51d5f243f9df9
santiago16171 <saantumb@espol.edu.ec>
2024-07-13 21:42:18 -0500

    login selection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginSelectionComponent } from './login-selection/login-selection.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //create route /. which will show the Login-SelectionComponent
+    {
+        path: '',
+        component: LoginSelectionComponent
+    }
+
+];
---
+++ src/app/login-selection/login-selection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-selection',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-selection.component.html',
+  styleUrl: './login-selection.component.css'
+})
+export class LoginSelectionComponent {
+
+}
