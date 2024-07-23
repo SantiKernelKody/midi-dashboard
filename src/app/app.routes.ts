d4ef6bd91132dfe371043387ac53fc3abf1b55fc
santiago16171 <saantumb@espol.edu.ec>
2024-07-23 14:48:33 -0500

    cahnging structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { LoginSelectionComponent } from './login-selection/login-selection.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-import { LayoutComponent } from './layout/layout.component';
 
 export const routes: Routes = [
     //create route /. which will show the Login-SelectionComponent
